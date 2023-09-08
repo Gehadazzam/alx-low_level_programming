6f4788644a3054e9261b50de0f00e579c17c2db6
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-07 17:12:31 -0700

    Helllo C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
-	printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
-	printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
-	printf("Size of Long Long Int Data Types in C = %2d bytes \n", sizeof(long long int));
-	printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
+	printf("Size of Char Data Types in C = %zu bytes \n", sizeof(char));
+	printf("Size of Int Data Types in C = %zu bytes \n", sizeof(short int));
+	printf("Size of Long Int Data Types in C = %zu bytes \n", sizeof(long int));
+	printf("Size of Long Long Int Data Types in C = %zu bytes \n", sizeof(long long int));
+	printf("Size of Float Data Types in C = %zu bytes \n", sizeof(float));
 	return (0);
 }
