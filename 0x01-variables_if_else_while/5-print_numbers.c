25ad1cbd536d35a67b05b934179e24fb983d473b
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-08 16:43:40 -0700

    learn C programing

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Uses the function "putchar" to print single digit
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char ch = '0'
+	for (ch = '0' ; ch <= '9' ; ch++)
+{
+		putchar(ch);
+}
+	putchar('\n');
+	return (0);
+}
