d75977c59b3525c93702cdc5cc0e013f547aa585
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-07 17:07:58 -0700

    Helllo C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Uses the function "printf" to print the size of data
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of Char Data Types in C = %2d bytes \n", sizeof(char));
+	printf("Size of Int Data Types in C = %2d bytes \n", sizeof(short int));
+	printf("Size of Long Int Data Types in C = %2d bytes \n", sizeof(long int));
+	printf("Size of Long Long Int Data Types in C = %2d bytes \n", sizeof(long long int));
+	printf("Size of Float Data Types in C = %2d bytes \n", sizeof(float));
+	return (0);
+}
