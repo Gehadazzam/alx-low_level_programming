f3a875c4d59d6bb3d9b6d2f836c830088dad91d2
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-08 14:32:29 -0700

    learn C programing

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Description: Uses the function "putchar" to print the alphapet
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+		putchar(ch);
+}
+	return (0);
+}
