7a00042688e804c16bbb8203f64e106e38b3f99c
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-14 14:33:34 -0700

    learn C programing

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Uses the function "_isupper" to detrmain the uppercase
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+/**
+*Returns 1 if c is uppercase
+*Returns 0 otherwise
+*c - Carachter to check
+*/
+int _isupper(int c);
+int c;
+	if (c >= 'A' && c <= 'Z')
+{
+	return (1);
+}
+	else
+{
+		return (0);
+}
+	return (0);
+}
