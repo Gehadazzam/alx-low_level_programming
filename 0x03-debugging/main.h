4a817ed92160aed8566faec140c69e700770bfb4
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-15 13:51:19 -0700

    learn C programing

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,6 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
+/*largest_number to determain the largest number*/
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
