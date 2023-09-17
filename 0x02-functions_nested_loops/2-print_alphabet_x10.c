d490f2968130cc388d5a0a02586126c4376849b7
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-17 14:25:27 -0700

    learn C programing

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#ifndef MAIN
+#define MAIN
+#include "main.h"
+/**
+*print_alphabet - Print the alphapet
+*
+*@ch - character to be checked
+*
+* Return: (0) success
+*/
+#endif
+void print_alphabet_x10(void)
+{
+char ch;
+int i=0;
+	while (i <= 9)
+{
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+		_putchar(ch);
+}
+	_putchar('\n');
+	i++;
+}
+}
