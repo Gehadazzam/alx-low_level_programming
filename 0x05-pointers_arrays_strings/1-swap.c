464672a4a034e6dccd6ec68b51fd4c0ece12096a
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-18 05:09:39 -0700

    learn C programing

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*swap_int - swap the value of two function
+*
+* @a: first integer
+*
+* @b: second integer
+*
+*Return: (0) always
+*/
+void swap_int(int *a, int *b)
+{
+*a = 42;
+*b = 98;
+}
