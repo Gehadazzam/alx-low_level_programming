024a4cddbad238b9aaac84678a5cc4e9d36f1862
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-23 13:01:38 -0700

    learn C programing

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,12 +11,14 @@ void print_to_98(int n)
 {
 	for (; n <= 98; n++)
 {
-		if (n < 98)
-			printf("%d, ", n);
-		else
+		if (n == 98)
 {
 			printf("%d\n", n);
 			break;
+}
+		else
+{
+			printf("%d, ", n);
 }
 }
 	for (; n >= 98; n--)
