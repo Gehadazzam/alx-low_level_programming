3f3d981680f3a1960ba1c7085797df5d94672459
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-25 11:21:28 -0700

    learn C programing

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+**_strchr - locates a character in a string
+*
+*@*s: pointer to the char
+*
+*@c: char to be located
+*
+*Return: (*s)
+*/
+char *_strchr(char *s, char c)
+{
+int i;
+int size;
+size = size_of *s / size_of c
+	for (i = 0; i < size; i++)
+{
+	if (c = a[i])
+{
+	s == a[i];
+}
+	else
+{
+	s == '\0';
+}
+}
