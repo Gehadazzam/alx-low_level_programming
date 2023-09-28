202068b65f55e9c2b1de00c2ad1e60920fb95523
Gehad azzam <gehadgamalazzam@gmail.com>
2023-09-28 13:23:41 -0700

    learn C programing

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,12 +1,37 @@
 #include "main.h"
 /**
-*_sqrt_recursion - returns the natural square root of a number
+*square_number - find the square root
+*
+* @n: integer to be checked
+*
+*@z: integer to check
+*
+*Return: (squarenumber)
+*/
+int square_number(int n, int z)
+{
+	if (z * z > n)
+{
+		return (-1);
+}
+	else if (z * z == n)
+{
+		return (z);
+}
+	return (square_number(n, z + 1));
+}
+/**
+*_sqrt_recursion - return the square root
 *
 *@n: integer to be checked
 *
-*Retuen: (number) or(-1)
+*Return: (number) or(-1)
 */
 int _sqrt_recursion(int n)
 {
-
+	if (n < 0)
+{
+		return (-1);
+}
+	return (square_number(n, 0));
 }
