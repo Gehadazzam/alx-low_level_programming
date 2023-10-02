2b8f15f1f0b15718683a4a44f37ce8f29cc364d4
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-02 05:45:39 -0700

    learn C programing

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,42 +10,26 @@
 *
 * Return: (0)
 */
-int main(int argc, char *argv[]);
+int main(int argc, char *argv[])
 {
-int i, sum = 0;
+int i, j, sum = 0;
 	if (argc < 2)
 		printf("0\n");
 	else
 {
 	for (i = 1; i < argc; i++)
 {
-		if (isNumeric(argv[i]))
+		for (j = 0; argv[i][j] != '\0'; j++)
 {
-			sum += atoi(argv[i]);
-			printf("%d\n", sum);
-}
-		else
+			if (argv[i][j] < '0' || argv[i][j] > '9')
 {
-			printf("Error\n");
-			return (1);
+				printf("Error\n");
+				return (1);
 }
+}
+		sum += atoi(argv[i]);
+}
+		printf("%d\n", sum);
 }
 	return (0);
-}
-/**
-*isNumeric - check if the input is number
-*
-*@str: pointer for the string
-*
-*Return: (1)
-*/
-int isNumeric(char *argv)
-{
-	while (*argv != '\0')
-{
-		if (*argv < '0' || *argv > '9')
-			return (0);
-		argv++;
-}
-	return (1);
 }
