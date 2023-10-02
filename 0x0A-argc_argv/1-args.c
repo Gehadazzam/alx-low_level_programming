949ca8ba11ab8c9b11f6855b0b8e6697bc385a0a
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-02 06:16:11 -0700

    learn C programing

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,9 +9,10 @@
 *
 * Return: (0)
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf ("%d\n", argc);
+(void)*argv;
+	printf("%d\n", argc);
 	return (0);
 }
 
--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -9,8 +9,12 @@
 *
 * Return: (0)
 */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	printf ("%s\n", argv);
+int i;
+	for (i = 0; i < argc; i++)
+{
+	printf("%s\n", argv[i]);
+}
 	return (0);
 }
