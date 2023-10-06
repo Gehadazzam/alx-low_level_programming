587af7a15a2168e65f9e7f5e64c2176adfb8705e
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-06 13:55:13 -0700

    learn C programing

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include "main.h"
-#include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 /**
 *main - check for multiplies two positive numbers
@@ -9,8 +9,37 @@
 *
 *Return: (void)
 */
-int main(int num1, int num2)
+int main(int argc, char *argv[])
 {
-int mul;
+int mul, i, num1, num2;
+char x = "Error";
+	if (argc != 3)
+{
+		_putchar(x);
+		_putchar('\n');
+		exit (98);
+}
+	for (i = 0; argv[1][i] != '\0'; i++)
+{
+		if (!isdigit(argv[1][i])
+{
+			_putchar(x);
+			_putchar('\n');
+			exit (98);
+}
+}
+	for (i = 0; argv[2][i] != '\0'; i++)
+{
+		if (!isdigit(argv[2][i])
+{
+			_putchar(x);
+			_putchar('\n');
+			exit (98);
+}
+}
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+mul = num1 * num2;
+	_putchar(mul);
 	return (0);
 }
