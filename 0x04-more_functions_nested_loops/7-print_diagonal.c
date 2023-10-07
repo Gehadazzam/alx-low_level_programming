658bffed645905b24704075af0b814ff528d2e6f
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-07 05:20:18 -0700

    learn C programing

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+*print_diagonal - draw a diagonal line
+*
+*@n: the number of the \ to print
+*
+*Return: (void)
+*/
+void print_diagonal(int n)
+{
+int i;
+	if (n <= 0)
+{
+		_putchar('\\');
+		_putchar('\n');
+}
+	else
+{
+		for (i = 0; i < n; i++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+}
+}
