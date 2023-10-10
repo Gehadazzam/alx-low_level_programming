1e430041cd7cec5a19d8f45c4443f8a03b37db30
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-10 06:18:09 -0700

    learn C programing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "dog.h"
-#include <stdlib.h>
 /**
 *print_dog - print what's inside the struct
 *@d: pointer to the struuct
@@ -11,9 +10,7 @@ void print_dog(struct dog *d)
 {
 	if (d == NULL)
 		return;
-	if (d->name == NULL)
-		d->name = "(nil)";
-	else if (d->owner == NULL)
-		d->owner = "(nil)";
+	d->name = (name == NULL) ? "(nil)" : name;
+	d->owner = (owner == NULL) ? "(nil)" : owner;
 	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
