96c99ac498928016afe5056ff4ef2d05e622a3ec
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-10 05:51:51 -0700

    learn C programing

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,5 +14,5 @@ void print_dog(struct dog *d)
 		printf("(nil)");
 	else if (d->owner == NULL)
 		printf("(nil)");
-	printf("Name: %s\nage: %f\nowner: %s\n", d->name, d->age, d->owner);
+	printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
 }
