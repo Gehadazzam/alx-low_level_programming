8cf6325ecd0cf85ad94454d8a431ef6f9b1f4345
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-11 10:51:44 -0700

    learn C programing

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+/**
+*print_name - contain pinter to function can print a name
+*
+*@name: the name to print
+*
+*@f: pointer to the function
+*
+*Return: (void)
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+	exit;
+}
