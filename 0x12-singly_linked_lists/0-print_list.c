d6bbaf8aabfbea2fc4daa426a7b148bd86096ea4
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-19 13:37:35 -0700

    learn C programing

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,12 +13,12 @@ size_t print_list(const list_t *h)
 size_t count = 0;
 	while (h)
 {
-		if (h->str != NULL)
-			printf("%d %s\n", h->len, h->str);
-		else
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
+		else
+			printf("%d %s\n", h->len, h->str);
 	count++;
-		h = h->next;
+	h = h->next;
 }
 	return (count);
 }
