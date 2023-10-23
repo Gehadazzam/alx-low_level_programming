2944e0c5e46acacce123a130d6895cbb31a8b603
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-23 11:24:32 -0700

    learn C programing

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*free_listint2 - function to free a memory allocated for a list
+*
+*@head: pointer to the list
+*
+*Return: void
+*/
+void free_listint2(listint_t **head)
+{
+listint_t *cat;
+	while (*head)
+{
+		cat = *head;
+		*head = (*head)->next;
+		free(cat);
+}
+	*head = NULL;
+}
