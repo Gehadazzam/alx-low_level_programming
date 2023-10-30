31e9677a62b57676582956d4ce9b09099962e3ce
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-30 12:31:09 -0700

    learn C programing

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <errno.h>
 /**
 *append_text_to_file - appened text to the end of the file
 *
@@ -10,5 +11,17 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-
+int cat, writing, length;
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		length = strlen(text_content);
+cat = open(filename, O_WRONLY | O_APPEND);
+	if (cat == -1)
+		return (-1);
+writing = write(cat, text_content, length);
+	if (writing == -1)
+		return (-1);
+	close(cat);
+	return (1);
 }
