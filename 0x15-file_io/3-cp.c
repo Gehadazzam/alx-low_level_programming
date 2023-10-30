fb57501a921e8e81e14255cc124bdd099496348c
Gehad azzam <gehadgamalazzam@gmail.com>
2023-10-30 15:32:58 -0700

    learn C programing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,29 +19,23 @@ int main(int argc, char *argv[])
 {
 char cat[BUFFER_kitty];
 ssize_t reading, writing;
-int f1, f2;
+int f1 = open(argv[1], O_RDONLY);
+int f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 const char *cat1 = argv[1], *cat2 = argv[2];
 	if (argc != 3)
 {
 		print_massage("Usage: cp file_from file_to");
 		exit(97);
 }
-	f1 = open(argv[1], O_RDONLY);
 		if (f1 == -1)
 {
 			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", cat1);
 			exit(98);
 }
-	f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-		if (f2 == -1)
-{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", cat2);
-			exit(99);
-}
 	while ((reading = read(f1, cat, BUFFER_kitty)) > 0)
 {
 		writing = write(f2, cat, reading);
-			if (writing == -1)
+			if (f2 == -1 || writing == -1)
 {
 				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", cat2);
 				exit(99);
