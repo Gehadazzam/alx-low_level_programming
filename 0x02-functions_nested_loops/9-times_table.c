7847cef8814c904c4942ba6ac6c255013067c49b
gehad <gehadgamalazzam@gmail.com>
2023-11-24 22:14:44 +0200

    learn C programing

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+*times_table - print a tme table
+*
+*Return: void
+*/
+void times_table(void)
+{
+int i, j, num;
+	for (i = 1; i <= 9; i++)
+{
+		for (j = 0; j <= 9; j++)
+{
+			num = j * i;
+			if (num < 10)
+{
+				_putchar('0' + num);
+}
+			else
+{
+				_putchar('0' + num / 10);
+				_putchar('0' + num % 10);
+}
+			if (j < 9)
+{
+			_putchar(',');
+}
+}
+		_putchar('\n');
+}
+}
