49e98eb34ed9c691fd5e9ba6855fe2793b8ad91c
gehad <gehadgamalazzam@gmail.com>
2023-11-24 19:07:34 +0200

    learn C programing

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("Size of Char Data Types in C = %zu byte(s) \n", sizeof(char));
-	printf("Size of Int Data Types in C = %zu byte(s) \n", sizeof(int));
-	printf("Size of Long Int Data Types in C = %zu byte(s) \n", sizeof(long int));
-	printf("Size of Long Long Int Data Types in C = %zu byte(s) \n", sizeof(long long int));
-	printf("Size of Float Data Types in C = %zu byte(s) \n", sizeof(float));
+	printf("Size of a char = %zu byte(s) \n", sizeof(char));
+	printf("SSize of an int = %zu byte(s) \n", sizeof(int));
+	printf("Size of a long int = %zu byte(s) \n", sizeof(long int));
+	printf("Size of a long long int = %zu byte(s) \n", sizeof(long long int));
+	printf("Size of a float = %zu byte(s) \n", sizeof(float));
 	return (0);
 }
