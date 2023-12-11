4367dbc6aa18f855c1689eeef6adee3df73d7606
gehad <gehadgamalazzam@gmail.com>
2023-12-11 20:41:33 +0200

    lib

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libgiga.so https://raw.githubusercontent.com/Gehadazzam/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
-export LD_PRELOAD="tmp/libgiga.so"
+wget -P /tmp https://github.com/Gehadazzam/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
+export LD_PRELOAD=/tmp/libgiga.so
