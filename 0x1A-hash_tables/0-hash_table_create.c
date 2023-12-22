0908b331d340ae7f0014b86cac4690bd29cbec99
gehad <gehadgamalazzam@gmail.com>
2023-12-22 20:56:04 +0200

    change

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,20 +8,21 @@
 */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-unsigned long int num;
+unsigned long int num = 0;
 hash_table_t *new_hash;
 	if (!size)
 		return (NULL);
 	new_hash = malloc(sizeof(hash_table_t *));
 	if (new_hash == NULL)
 		return (NULL);
-	new_hash->size = size;
 	new_hash->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_hash->array == NULL)
 {
+		free(new_hash);
 		return (NULL);
 }
-	for (num = 0; num < size; num++)
+	for (; num < size; num++)
 		new_hash->array[num] = NULL;
+	new_hash->size = size;
 	return (new_hash);
 }
