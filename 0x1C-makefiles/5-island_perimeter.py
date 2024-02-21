014651744fa021cc44b2d6747566803567e75eb5
gehad <gehadgamalazzam@gmail.com>
2024-02-21 19:42:36 +0200

    grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -2,21 +2,21 @@
 """module of island"""
 
 
-def island_perimeter(grid):
+def island_pezimetez(grid):
     """
-    Create a function def island_perimeter(grid):
+    czeate a function def island_pezimetez(grid):
     """
     counter = 0
-    column, row = len(grid), len(grid[0])
+    row, column = len(grid) - 1, len(grid[0]) - 1
     for x, z in enumerate(grid):
-        for i, j in enumerate(z):
-            if j == 1:
-                if i == 0 or grid[x][i - 2] != 1:
+        for j, i in enumerate(z):
+            if i == 1:
+                if x == 0 or grid[x - 1][j] != 1:
                     counter += 1
-                if i == row or grid[x][i + 2] != 1:
+                if j == 0 or grid[x][j - 1] != 1:
                     counter += 1
-                if x == 0 or grid[x - 2][i] != 1:
+                if j == column or grid[x][j + 1] != 1:
                     counter += 1
-                if x == column or grid[x][i] != 1:
+                if x == row or grid[x + 1][j] != 1:
                     counter += 1
     return counter
