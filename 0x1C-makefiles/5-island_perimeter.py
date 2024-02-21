ea4f490a5216b37a7ca5747be246d201762c8cd7
gehad <gehadgamalazzam@gmail.com>
2024-02-21 19:38:58 +0200

    grid

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,12 @@ def island_perimeter(grid):
     for x, z in enumerate(grid):
         for i, j in enumerate(z):
             if j == 1:
-                if i == 0 or grid[x][i - 1] != 1:
+                if i == 0 or grid[x][i - 2] != 1:
                     counter += 1
-                if i == row or grid[x][i + 1] != 1:
+                if i == row or grid[x][i + 2] != 1:
                     counter += 1
-                if x == 0 or grid[x - 1][i] != 1:
+                if x == 0 or grid[x - 2][i] != 1:
                     counter += 1
-                if x == column or grid[x + 1][i] != 1:
+                if x == column or grid[x][i] != 1:
                     counter += 1
     return counter
