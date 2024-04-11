fba42184e8e804f27b4f1b3b4a11a3fa4beb9373
gehad <gehadgamalazzam@gmail.com>
2024-04-12 00:08:48 +0200

    jumping

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,32 +1,19 @@
 #include "search_algos.h"
 #include <math.h>
 /**
-*print_array - Prints an array of integers
-*
-*@array: Pointer to the first element of the array
-*@left: Index of the left boundary of the subarray to print
-*@right: Index of the right boundary of the subarray to print
-*/
-void print_array(int *array, size_t left, size_t right)
-{
-	printf("Value checked array[%lu] = [%d]\n", left, array[left]);
-	printf("Value checked array[%lu] = [%d]\n", right, array[right]);
-}
-
-/**
-*jump_search - Searches for a value in a sorted array of integers
-*
-*@array: Pointer to the first element of the array
-*@size: Number of elements in the array
-*@value: Value to search for
-*
-*Return: Index where value is located, or -1 if not found
-*/
+ * jump_search - Searches for a value in a sorted array of integers
+ *
+ * @array: Pointer to the first element of the array to search in
+ * @size: Number of elements in the array
+ * @value: Value to search for
+ *
+ * Return: Index where value is located, or -1 if not found
+ */
 int jump_search(int *array, size_t size, int value)
 {
-	size_t i, step, right, left = 0;
+	size_t step, right, i, left = 0;
 
-	if (array == NULL || !size || !value)
+	if (array == NULL || size == 0)
 		return (-1);
 
 	step = sqrt(size);
@@ -34,16 +21,14 @@ int jump_search(int *array, size_t size, int value)
 
 	while (right < size && array[right] < value)
 	{
-		print_array(array, left, right);
+		printf("Value checked array[%lu] = [%d]\n", right, array[right]);
 		left = right;
 		right += step;
 	}
 
-	print_array(array, left, right);
-
 	printf("Value found between indexes [%lu] and [%lu]\n", left, right);
 
-	for (i = left; i <= right && i < size; i++)
+	for (i = left; i <= (right < size ? right : size - 1); i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
@@ -52,3 +37,4 @@ int jump_search(int *array, size_t size, int value)
 
 	return (-1);
 }
+
